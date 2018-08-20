bae89eb66f86de07214d6cf92abf6ae94136e356
shonesque <simion.cosmin89@gmail.com>
2018-08-20 17:41:08 +0300

    Added the Mini CSS Extract Plugin

---
+++ src/scripts/firebase-config.js
@@ -0,0 +1,9 @@
+export let config = {
+    apiKey: "AIzaSyCowgpQ5gOi9BFlxFtneGtigu_HBDGqrPc",
+    authDomain: "portfolio-d8844.firebaseapp.com",
+    databaseURL: "https://portfolio-d8844.firebaseio.com",
+    projectId: "portfolio-d8844",
+    storageBucket: "portfolio-d8844.appspot.com",
+    messagingSenderId: "498654877523"
+  };
+  firebase.initializeApp(config);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let path = require('path');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+let OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+// related to TODO1 bellow
+// let svgBG = require('svg-inline-loader?classPrefix!./src/img/bg.svg');
 
 module.exports  = {
+    optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true // set to true if you want JS source maps
+          }),
+          new OptimizeCssAssetsPlugin({})
+        ]
+      },
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, './dist'),
@@ -9,19 +25,42 @@ module.exports  = {
     },
     module: {
         rules: [
-                {
-                    test: /\.css$/,
-                    use: [ 'style-loader', 'css-loader' ]
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  // you can specify a publicPath here
+                  // by default it use publicPath in webpackOptions.output
+                  publicPath: './dist'
                 }
-                
+              },
+              "css-loader"
+            ]
+          } 
+        //   TODO1: Find a solution for using SVG files. The Loader bellow seems not to be working or needs more configuration.
+        //   {
+        //     test: /\.svg$/,
+        //     loader: 'svg-inline-loader'
+        // }
         ]
-    },
-    plugins: [new HtmlWebpackPlugin({
+      },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'app.css'
+            }),
+
+        new HtmlWebpackPlugin({
         title: 'Portfolio | Cosmin Simion',
         minify: {
             collapseWhitespace: true
         },
         hash:true,
         template: 'src/index.html'
-    })]
-}
+        
+        })
+    ]
+}   
